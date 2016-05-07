49acf422c28a474002ca9bb50d4fe26ec22882ea
Brett <brett@applogix.com>
2016-05-07 13:45:43 -0700

    Complete rendering Grocery list items dynamically based on array of grocery items

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// TODO
+class GroceryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ['kale', 'spinach', 'tomatoes', 'rice']
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h2>Grocery List</h2>
+        {this.state.items.map((item) => (
+          <GroceryListItem item={item}/>
+        ))}
+      </div>
+    );
+  }
+};
+
+var GroceryListItem = (props) => (
+  <div>{props.item}</div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
